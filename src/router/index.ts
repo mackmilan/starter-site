56ad4342e5f08b9adb015d3cfb428d43919794e6
Andre Silva <andre.liomar@softvaro.com.br>
2026-08-26 21:15:57 -0300

    Remove unused views: FundamentacaoView, NoticiasView, and SobreView

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,56 +1,26 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory } from "vue-router";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    component: () => import('../views/HomeView.vue')
+    path: "/",
+    name: "home",
+    component: () => import("../views/HomeView.vue"),
   },
   {
-    path: '/sobre',
-    name: 'sobre',
-    component: () => import('../views/SobreView.vue')
+    path: "/:pathMatch(.*)*",
+    redirect: "/",
   },
-  {
-    path: '/fundamentacao',
-    name: 'fundamentacao',
-    component: () => import('../views/FundamentacaoView.vue')
-  },
-  {
-    path: '/como-implantar',
-    name: 'como-implantar',
-    component: () => import('../views/ComoImplantarView.vue')
-  },
-  {
-    path: '/noticias',
-    name: 'noticias',
-    component: () => import('../views/NoticiasView.vue')
-  },
-  {
-    path: '/contato',
-    name: 'contato',
-    component: () => import('../views/ContatoView.vue')
-  },
-  {
-    path: '/apoie',
-    name: 'apoie',
-    component: () => import('../views/ApoieView.vue')
-  },
-  {
-    path: '/:pathMatch(.*)*',
-    redirect: '/'
-  }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
   scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
-      return savedPosition
+      return savedPosition;
     }
-    return { top: 0 }
-  }
-})
+    return { top: 0 };
+  },
+});
 
-export default router
+export default router;
